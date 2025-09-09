932180589866bc3bbf7d75d869dd812d432a34bb
adi9113 <adrian.desiderato.ad@gmail.com>
2025-09-09 20:47:13 +0200

    fix: apply temp fix for current bug with bufferline and catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,4 +3,14 @@ return {
   opts = {
     colorscheme = "catppuccin",
   },
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
 }
