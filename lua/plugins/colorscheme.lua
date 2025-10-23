0143a10a977fd946aeb15527ee19de679433a5db
adi9113 <adrian.desiderato.ad@gmail.com>
2025-10-23 11:51:36 +0200

    chore: remove temp fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,4 @@ return {
   opts = {
     colorscheme = "catppuccin",
   },
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
 }
